3fefa006cf37ab2399dcf3db91587ffa420d64e2
Jacob Thornton <jacobthornton@gmail.com>
2011-09-12 22:45:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+DEAL WITH IT
+============
+
+http://f.cl.ly/items/2u3h2k2s2e3l423Q2i0X/Screen%20shot%202011-09-10%20at%2010.05.41%20PM.png
---
+++ js/app.js
@@ -0,0 +1,63 @@
+!function () {
+  $(init)
+
+  var host = 'localhost:3000'
+    , $container
+    , $message
+    , $img
+
+  function init() {
+      $img = $("img")
+      $container = $("#container").hide()
+      $message = $('#message').hide().text(getUrlParam('message') || 'deal with it')
+      attachEvents()
+      $img.attr('src', 'img/face.jpg')
+  }
+
+  function getUrlParam(name) {
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]")
+    var regexS = "[\\?&]"+name+"=([^&#]*)"
+      , regex = new RegExp(regexS)
+      , results = regex.exec(location.href)
+
+    return results ? results[1] : ''
+  }
+
+  function attachEvents() {
+    return $img
+      .error(function () {
+        return alert('sorry -- looks like your image cannot be processed :(')
+      })
+      .load(function () {
+         $container.fadeIn(function () {
+            var coords = $img.faceDetection({
+              complete: function (img, coords) {
+                if (!coords.length) {
+                  return alert('sorry -- looks like your image cannot be processed :(')
+                }
+                dealWithIt(coords[0])
+              },
+              error: function () {
+                return alert('sorry -- looks like your image cannot be processed :(')
+              }
+            })
+         })
+       })
+  }
+
+  function dealWithIt(coords) {
+    $('<img id="glasses" src="/img/glasses.png" />')
+      .appendTo($container)
+      .show()
+      .css({
+        'position': 'absolute'
+      , 'top': -50
+      , 'left': coords.offsetX - (coords.width/3)
+      , 'width': coords.width + (coords.width/3)
+      })
+      .animate({ top: coords.offsetY + (coords.height/3) }, 5000, function () {
+        $('#message').show()
+      })
+  }
+
+}()
